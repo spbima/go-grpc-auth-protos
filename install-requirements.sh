6d2000220aae24f4d24f59cc2b13d22a023a210f
Ivanko Mike <ivanko@iac.spb.ru>
2023-11-28 15:02:56 +0300

    Init commit with proto file for auth

---
+++ install-requirements.sh
@@ -0,0 +1,9 @@
+# Install Protoc util
+sudo snap install protobuf --classic
+
+# Install plugins
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+
+# Add path
+export PATH="$PATH:$HOME/go/bin"
